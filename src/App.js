ba02e5a8c00a58ecd6f4283936dfbb03910199d9
Surya Setiyaputra <hello@setiyaputra.me>
2017-05-23 14:37:15 +1000

    test different APIs for components of dashboard

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import './App.css';
 import ReactMapboxGl, { Layer, Feature, ScaleControl, ZoomControl, Marker } from "react-mapbox-gl";
 import config from "./config.json";
 import style from "./style.json";
+import route from "./route.json";
+import axios from 'axios';
+
 
 const { accessToken } = config;
 
@@ -18,48 +21,118 @@ const markerCoord = [
   -34.4123709344377
 ];
 
+const params = {v: '3.exp', key: 'AIzaSyDX8-TRWtqBRRAegtMRVrbfR7-u_bLnP9o'};
+
+const mappedRoute = route.points.map(point => [ point.lat, point.lng ]);
+
 class App extends Component {
   intervalHandle = null
   timeoutHandle = null
   mounted = false
 
-  state = {
-    popup: null,
-    center: [151.3276046,-34.0900158],
-    circleRadius: 30,
-    routeIndex: 0
-  };
+  constructor(props) {
+  super(props);
+  this.state = {
+      latitude: 0,
+      longitude: 0,
+      popup: null,
+      center: [151.3276046,-34.0900158],
+      circleRadius: 30,
+      routeIndex: 0
+    };
+  }
 
   componentWillMount() {
-    this.mounted = true;
-    this.timeoutHandle = setTimeout(() => {
-      if (this.mounted) {
-        this.setState({
-          center: [151.3276046,-34.0900158],
-          circleRadius: 10
-        });
-      }
-    }, 3000);
+    // this.mounted = true;
+    this.getISSPosition();
 
-    this.intervalHandle = setInterval(() => {
-      if (this.mounted) {
+
+    // this.timeoutHandle = setTimeout(() => {
+    //   if (this.mounted) {
+    //     this.setState({
+    //       center: [151.3276046,-34.0900158],
+    //       circleRadius: 10
+    //     });
+    //   }
+    // }, 3000);
+    //
+    // this.intervalHandle = setInterval(() => {
+    //   if (this.mounted) {
+    //     this.setState({
+    //       routeIndex: this.state.routeIndex + 1
+    //     });
+    //   }
+    // }, 8000);
+  };
+
+  getISSPosition() {
+    setInterval(() => {
+      axios.get(`http://api.open-notify.org/iss-now.json`)
+      .then(result => {
         this.setState({
-          routeIndex: this.state.routeIndex + 1
-        });
-      }
-    }, 8000);
+          latitude: result.data.iss_position.latitude,
+          longitude: result.data.iss_position.longitude
+        })
+        console.log(this.state.latitude);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }, 5000);
+  }
+  //
+  // componentWillUnmount() {
+  //   this.mounted = false;
+  //   clearInterval(this.intervalHandle);
+  //   clearTimeout(this.timeoutHandle);
+  // }
+
+  _onClickMarker = ({ feature }) => {
+    this.setState({
+      center: feature.geometry.coordinates
+    });
+  };
+
+  _onClickMap(map) {
+    console.log("Clicked on the map : ", map);
   }
 
-  componentWillUnmount() {
-    this.mounted = false;
-    clearInterval(this.intervalHandle);
-    clearTimeout(this.timeoutHandle);
+  _onStyleLoad(map) {
+    console.log("Style loaded: ", map);
   }
+
+  _onHover({ map }) {
+    map.getCanvas().style.cursor = "pointer";
+  }
+
+  _onZoom(map) {
+    console.log("Zoom level changed to ", map.getZoom());
+  }
+
+  _onEndHover({ map }) {
+    map.getCanvas().style.cursor = "";
+  }
+
+  _markerClick() {
+    console.log('Marker clicked');
+  }
+
+  _markerMouseEnter() {
+    console.log('Marker mouse enter');
+  }
+
+  _markerMouseLeave() {
+    console.log('Marker mouse leave');
+  }
+
+  _polygonClicked = ({ feature }) => {
+    console.log("Polygon clicked", feature.geometry.coordinates);
+  };
 
   render() {
     return (
       <ReactMapboxGl
-        style={config.style}
+        style={style}
         zoom={[9]}
         onClick={this._onClickMap}
         onZoom={this._onZoom}
@@ -80,6 +153,27 @@ class App extends Component {
             onHover={this._onHover}
             onEndHover={this._onEndHover}
             onClick={this._onClickMarker}/>
+        </Layer>
+
+
+        <Layer
+          id="mapbox-route-example"
+          type="line"
+          sourceId="route"
+          layout={{
+            "line-join": "round",
+            "line-cap": "round"
+          }}
+          paint={{
+            "line-color": "#888",
+            "line-width": 8
+          }}/>
+
+        <Layer
+          type="line"
+          layout={{ "line-cap": "round", "line-join": "round" }}
+          paint={{ "line-color": "#4790E5", "line-width": 12 }}>
+          <Feature coordinates={mappedRoute}/>
         </Layer>
 
         <Marker
---
+++ src/RenderMap.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+
+//import component
+import ReactMapboxGl, { Layer, Feature, ScaleControl, ZoomControl, Marker } from "react-mapbox-gl";
+import config from "./config.json";
+import style from "./style.json";
+
+const { accessToken } = config;
+
+const containerStyle = {
+  height: "100vh",
+  width: "100%"
+};
+
+const markerCoord = [
+  150.89143690014,
+  -34.4123709344377
+];
+
+class RenderMap extends Component{
+  intervalHandle = null
+  timeoutHandle = null
+  mounted = false
+
+  state = {
+    popup: null,
+    center: [151.3276046,-34.0900158],
+    circleRadius: 30,
+    routeIndex: 0
+  };
+
+  componentWillMount() {
+    this.mounted = true;
+    this.timeoutHandle = setTimeout(() => {
+      if (this.mounted) {
+        this.setState({
+          center: [151.3276046,-34.0900158],
+          circleRadius: 10
+        });
+      }
+    }, 3000);
+
+    this.intervalHandle = setInterval(() => {
+      if (this.mounted) {
+        this.setState({
+          routeIndex: this.state.routeIndex + 1
+        });
+      }
+    }, 8000);
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+    clearInterval(this.intervalHandle);
+    clearTimeout(this.timeoutHandle);
+  }
+
+  return (){
+    <ReactMapboxGl
+      style={config.style}
+      zoom={[9]}
+      onClick={this._onClickMap}
+      onZoom={this._onZoom}
+      onStyleLoad={this._onStyleLoad}
+      accessToken={accessToken}
+      center={this.state.center}
+      movingMethod="jumpTo"
+      containerStyle={containerStyle}>
+
+      <ScaleControl/>
+      <ZoomControl/>
+
+      <Layer
+        type="symbol"
+        layout={{ "icon-image": "marker-15" }}>
+        <Feature
+          coordinates={markerCoord}
+          onHover={this._onHover}
+          onEndHover={this._onEndHover}
+          onClick={this._onClickMarker}/>
+      </Layer>
+
+      <Marker
+        onClick={this._markerClick}
+        anchor="top"
+        onMouseEnter={this._markerMouseEnter}
+        onMouseLeave={this._markerMouseLeave}
+        className="station"
+        coordinates={markerCoord}>
+      </Marker>
+
+    </ReactMapboxGl>
+  };
+}
+
+export default RenderMap;
