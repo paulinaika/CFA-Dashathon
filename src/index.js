37d089bc937ae6c46eec2c5b4e81161953dc2448
paulinaika <paulina.ika@gmail.com>
2017-05-25 00:10:25 +1000

    added gif.css/gif.js and modified index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App2';
+import App from './Gif';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
